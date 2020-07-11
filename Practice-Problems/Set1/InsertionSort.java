9179d4f031730cdb13d015edaf3f57cb6438342a
Abhishek Keshri <keshri.abhishek63@gmail.com>
2020-07-11 10:31:17 +0530

    Add InsertionSort.java

---
+++ Practice-Problems/Set1/InsertionSort.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class InsertionSort {
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		System.out.print("Enter array size : ");
+		int[] arr = new int[scan.nextInt()];
+		System.out.println("Enter the elments : ");
+		for (int i = 0; i < arr.length; i++) {
+			arr[i] = scan.nextInt();
+		}
+		scan.close();
+		arr = insertionSort(arr);
+		System.out.println("After sorting : ");
+		for (int x: arr) System.out.print(x + ", ");
+	}
+
+	private static int[] insertionSort(int[] arr) {
+		int n = arr.length;
+		for (int i = 1; i < n; ++i) {
+			int curr = arr[i];
+			int j = i - 1;
+			while (j >= 0 && arr[j] > curr) {
+				arr[j+1] = arr[j];
+				j = j - 1;
+			}
+			arr[j + 1] = curr;
+		}
+		return arr;
+	}
+
+}
