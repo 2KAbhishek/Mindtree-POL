4fe7227d222e4470f1a73ae641afbed6c121b310
Abhishek Keshri <keshri.abhishek63@gmail.com>
2020-07-28 23:21:10 +0530

    Update VarArrSum.java

--- Practice-Problems/set1/VarArrSum.java
+++ Practice-Problems/set1/VarArrSum.java
@@ -1,37 +1,41 @@
-import java.util.Scanner;
+package set1;
 
 public class VarArrSum {
 
 	public static void main(String[] args) {
-		Scanner scan = new Scanner(System.in);
 		System.out.print("Enter no. of elements for 1st Array: ");
-		int size = scan.nextInt();
-		double [] arr = new double[size];
+		int size = Helper.getPosInt();
+		double[] arr = new double[size];
+		System.out.println("Enter " + size + " elements :");
 		for (int i = 0; i < size; i++) {
-			arr[i] = scan.nextInt();
+			arr[i] = Helper.getDouble();
 		}
-		
+
 		System.out.print("Enter no. of elements for 2nd Array: ");
-		int size2 = scan.nextInt();
-		double [] arr2 = new double[size2];
+		int size2 = Helper.getPosInt();
+
+		System.out.println("Enter " + size2 + " elements :");
+		double[] arr2 = new double[size2];
 		for (int i = 0; i < size2; i++) {
-			arr2[i] = scan.nextInt();
+			arr2[i] = Helper.getDouble();
 		}
-		scan.close();
-		
-		double[] result = new double[size>size2? size:size2];
-		
+
+		int[] result = new int[size > size2 ? size : size2];
+
 		for (int i = 0; i < arr.length || i < arr2.length; i++) {
-			if (i < arr.length && i < arr2.length) result[i] = arr[i] + arr2[i];
-			if (i > arr.length - 1) result[i] = arr2[i];
-			if (i > arr2.length - 1) result[i] = arr[i];
+			if (i < arr.length && i < arr2.length)
+				result[i] = (int) (arr[i] + arr2[i]);
+			if (i > arr.length - 1)
+				result[i] = (int) arr2[i];
+			if (i > arr2.length - 1)
+				result[i] = (int) arr[i];
 		}
-		
+
 		System.out.println("Summed array : ");
-		for (double n : result) {
+		for (int n : result) {
 			System.out.print(n + " ");
 		}
-		
+
 	}
 
 }
