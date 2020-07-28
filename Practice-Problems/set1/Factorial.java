e145e231fa77da430bac5bf88c3d181d2312b916
Abhishek Keshri <keshri.abhishek63@gmail.com>
2020-07-28 23:21:11 +0530

    Update Factorial.java

--- Practice-Problems/set1/Factorial.java
+++ Practice-Problems/set1/Factorial.java
@@ -1,16 +1,18 @@
-import java.util.Scanner;
+package set1;
 
 public class Factorial {
 
 	public static void main(String[] args) {
-		Scanner scan = new Scanner(System.in);
-		int num = scan.nextInt();
-		scan.close();
+		int num = Helper.getPosIntZero();
+		System.out.println("Factorial is : " + factorial(num));
+	}
+
+	private static long factorial(int num) {
 		long result = 1;
 		while (num > 0) {
 			result *= num--;
 		}
-		System.out.println("Factorial is : " + result);
+		return result;
 	}
 
 }
