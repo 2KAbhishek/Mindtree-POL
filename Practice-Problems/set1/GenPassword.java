1cbe55d316e34d4acac341aa017680066ac496fd
Abhishek Keshri <keshri.abhishek63@gmail.com>
2020-07-28 23:21:11 +0530

    Update GenPassword.java

--- Practice-Problems/set1/GenPassword.java
+++ Practice-Problems/set1/GenPassword.java
@@ -1,8 +1,28 @@
+package set1;
+
+import java.util.Scanner;
+
 public class GenPassword {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Scanner scan = new Scanner(System.in);
+		System.out.print("Enter full name : ");
+		String name = scan.nextLine();
+		System.out.print("Enter age : ");
+		int age = Helper.getPosInt();
+		scan.close();
+		String initials = "";
 
+		for (int i = 0; i < name.length(); i++) {
+			if (Helper.isUpperCase(name.charAt(i))) 
+				initials += name.charAt(i);
+		}
+
+		String password = initials + age;
+
+		while (password.length() < 8) {
+			password += age + initials;
+		}
+		System.out.println("Password : " + password);
 	}
-
 }
