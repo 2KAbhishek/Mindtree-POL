0fba7ac98f4bfef51118bf353a1521dd326a076b
Abhishek Keshri <keshri.abhishek63@gmail.com>
2020-07-28 23:21:11 +0530

    Update SwapLastName.java

--- Practice-Problems/set1/SwapLastName.java
+++ Practice-Problems/set1/SwapLastName.java
@@ -1,3 +1,5 @@
+package set1;
+
 import java.util.Scanner;
 
 public class SwapLastName {
@@ -9,26 +11,34 @@ public class SwapLastName {
 		String name2 = scan.nextLine();
 		scan.close();
 		String firstName1 = "", firstName2 = "", lastName1 = "", lastName2 = "";
-		
+
 		for (int i = 0; i < name1.length(); i++) {
 			char c = name1.charAt(i);
-			if (Character.isSpaceChar(c)){
-				firstName1 = name1.substring(0, i);
-				lastName1 = name1.substring(i);
+			if (c == ' ') {
+				firstName1 = subStr(0, i, name1);
+				lastName1 = subStr(i + 1, name1.length() - 1, name1);
 			}
 		}
 
 		for (int i = 0; i < name2.length(); i++) {
 			char c = name2.charAt(i);
-			if (Character.isSpaceChar(c)){
-				firstName2 = name2.substring(0, i);
-				lastName2 = name2.substring(i);
+			if (c == ' ') {
+				firstName2 = subStr(0, i, name2);
+				lastName2 = subStr(i + 1, name2.length() - 1, name2);
 			}
 		}
-		
+
 		String newName1 = firstName1 + lastName2;
 		String newName2 = firstName2 + lastName1;
 		System.out.println("After swapping : " + newName1 + " " + newName2);
+	}
+
+	private static String subStr(int begin, int end, String str) {
+		String out = "";
+		for (int i = begin; i <= end; i++) {
+			out += str.charAt(i);
+		}
+		return out;
 	}
 
 }
