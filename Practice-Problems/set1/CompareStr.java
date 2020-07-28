60341d25e0d1a4232f1e12e48427a4a338dc4d74
Abhishek Keshri <keshri.abhishek63@gmail.com>
2020-07-28 23:21:11 +0530

    Update CompareStr.java

--- Practice-Problems/set1/CompareStr.java
+++ Practice-Problems/set1/CompareStr.java
@@ -1,41 +1,50 @@
+package set1;
+
 import java.util.Scanner;
 
 public class CompareStr {
-	static String value = "";
-	public static void main(String[] args) {
-		Scanner scan = new Scanner(System.in);
-		System.out.println("Enter two strings : ");
-		value = scan.nextLine();
-		String str = scan.nextLine();
-		scan.close();
-		System.out.println("Comparing returned : " + compareTo(str));
+	char[] value = null;
+
+	CompareStr(char[] value) {
+		this.value = value;
 	}
-	
-	public static int compareTo(String anotherString) {
-		int len1,len2;
-		len1 = value.length();
-		len2 = anotherString.length();
-		
+
+	public int compareTo(CompareStr anotherString) {
+		int len1 = value.length;
+		int len2 = anotherString.value.length;
 		int lim = Math.min(len1, len2);
-		
-		char[] v1 = new char[len1];
-		char[] v2 = new char[len2];
-		
-		for(int i = 0;i < len1;i++)
-			v1[i] = value.charAt(i);
-		
-		for(int i = 0;i < len2; i++)
-			v2[i] = anotherString.charAt(i);
-		
+		char v1[] = value;
+		char v2[] = anotherString.value;
 		int k = 0;
-		while(k < lim) {
+		while (k < lim) {
 			char c1 = v1[k];
 			char c2 = v2[k];
-			if(c1 != c2) {
-				return c1-c2;
+			if (c1 != c2) {
+				return c1 - c2;
 			}
 			k++;
 		}
-		return len1-len2;
+		return len1 - len2;
+	}
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		System.out.println("Enter String 1:");
+		String s1 = scan.nextLine();
+		System.out.println("Enter String2:");
+		String s2 = scan.nextLine();
+		char[] value = new char[s1.length()];
+		char[] value1 = new char[s2.length()];
+
+		for (int i = 0; i < s1.length(); i++)
+			value[i] = s1.charAt(i);
+
+		for (int i = 0; i < s2.length(); i++)
+			value1[i] = s2.charAt(i);
+
+		CompareStr str = new CompareStr(value);
+		CompareStr str1 = new CompareStr(value1);
+		System.out.println("Strings differ by : " + str.compareTo(str1));
+		scan.close();
 	}
 }
