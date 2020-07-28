c68f967fe89952788270b00605141310aa2cbc38
Abhishek Keshri <keshri.abhishek63@gmail.com>
2020-07-28 23:21:11 +0530

    Update TestUSN.java

--- Practice-Problems/set1/TestUSN.java
+++ Practice-Problems/set1/TestUSN.java
@@ -1,3 +1,5 @@
+package set1;
+
 import java.util.Scanner;
 
 public class TestUSN {
@@ -7,18 +9,26 @@ public class TestUSN {
 		System.out.println("Enter University Serial Number : ");
 		String usn = scan.nextLine();
 		scan.close();
-		if (validUSN(usn)) System.out.println("Success");
-		else System.out.println("Failure");
+		if (validUSN(usn))
+			System.out.println("Success");
+		else
+			System.out.println("Failure");
 	}
 
 	private static boolean validUSN(String usn) {
 		String temp = usn.substring(5, 7);
-		if (usn.length() != 10) return false;
-		if (usn.charAt(0) != '1' && usn.charAt(0) != '2') return false;
-		if (!Character.isUpperCase(usn.charAt(1)) && !Character.isUpperCase(usn.charAt(2))) return false;
-		if (!Character.isDigit(usn.charAt(3)) && !Character.isDigit(usn.charAt(4))) return false;
-		if (!(temp.equals("CS") || temp.equals("IS") || temp.equals("EC") || temp.equals("ME"))) return false;
-		if (!Character.isDigit(usn.charAt(7)) && !Character.isDigit(usn.charAt(8)) && !Character.isDigit(usn.charAt(9))) return false;
+		if (usn.length() != 10)
+			return false;
+		if (usn.charAt(0) != '1' && usn.charAt(0) != '2')
+			return false;
+		if (!Character.isUpperCase(usn.charAt(1)) && !Character.isUpperCase(usn.charAt(2)))
+			return false;
+		if (!Character.isDigit(usn.charAt(3)) && !Character.isDigit(usn.charAt(4)))
+			return false;
+		if (!(temp.equals("CS") || temp.equals("IS") || temp.equals("EC") || temp.equals("ME")))
+			return false;
+		if (!Character.isDigit(usn.charAt(7)) && !Character.isDigit(usn.charAt(8)) && !Character.isDigit(usn.charAt(9)))
+			return false;
 		return true;
 	}
 
