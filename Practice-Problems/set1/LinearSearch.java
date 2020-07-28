5c18f557e6f87dbaf3a564ff324683988c83e4cf
Abhishek Keshri <keshri.abhishek63@gmail.com>
2020-07-28 23:21:11 +0530

    Update LinearSearch.java

--- Practice-Problems/set1/LinearSearch.java
+++ Practice-Problems/set1/LinearSearch.java
@@ -1,22 +1,23 @@
-import java.util.Scanner;
+package set1;
 
 public class LinearSearch {
 
 	public static void main(String[] args) {
-		Scanner scan = new Scanner(System.in);
 		System.out.print("Enter array size : ");
-		int[] arr = new int[scan.nextInt()];
+		int[] arr = new int[Helper.getPosInt()];
 		System.out.println("Enter the elments : ");
 		for (int i = 0; i < arr.length; i++) {
-			arr[i] = scan.nextInt();
+			arr[i] = Helper.getInt();
 		}
+		arr = Helper.bubbleSort(arr);
 		System.out.print("Enter element to search : ");
-		int search = scan.nextInt();
-		scan.close();
+		int search = Helper.getInt();
 		boolean found = linearSearch(search, arr);
-		
-		if (found) System.out.println(search + " found.");
-		else System.out.println(search + " not found");
+
+		if (found)
+			System.out.println(search + " found.");
+		else
+			System.out.println(search + " not found");
 	}
 
 	private static boolean linearSearch(int search, int[] arr) {
@@ -29,5 +30,5 @@ public class LinearSearch {
 		}
 		return exists;
 	}
-
+	
 }
