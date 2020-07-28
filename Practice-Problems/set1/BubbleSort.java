be2ad8e4c9f4e94c1387f0a8bd7f89da337067b7
Abhishek Keshri <keshri.abhishek63@gmail.com>
2020-07-28 23:21:11 +0530

    Update BubbleSort.java

--- Practice-Problems/set1/BubbleSort.java
+++ Practice-Problems/set1/BubbleSort.java
@@ -1,39 +1,36 @@
-import java.util.Scanner;
+package set1;
 
 public class BubbleSort {
 
 	public static void main(String[] args) {
-		Scanner scan = new Scanner(System.in);
 		System.out.print("Enter array size : ");
-		int size = scan.nextInt();
+		int size = Helper.getPosInt();
 		int[] arr = new int[size];
 		System.out.println("Enter elements :");
 		for (int i = 0; i < size; i++) {
-			arr[i] = scan.nextInt();
+			arr[i] = Helper.getInt();
 		}
-		scan.close();
 		arr = bubbleSort(arr);
 		System.out.println("Sorted array : ");
-		for (int x: arr) System.out.print(x + ", ");
+		for (int x : arr)
+			System.out.print(x + ", ");
 	}
 
 	private static int[] bubbleSort(int[] arr) {
 		int n = arr.length;
 		boolean swapHappened;
-		System.out.println("Intermediate Steps:");
-		for (int i = 0; i < n-1; i++) {
+		for (int i = 0; i < n - 1; i++) {
 			swapHappened = false;
-			for (int j = 0; j < n-i-1; j++) {
-				if (arr[j] > arr[j+1]) {
+			for (int j = 0; j < n - i - 1; j++) {
+				if (arr[j] > arr[j + 1]) {
 					int temp = arr[j];
-					arr[j] = arr[j+1];
-					arr[j+1] = temp;
+					arr[j] = arr[j + 1];
+					arr[j + 1] = temp;
 					swapHappened = true;
 				}
-				for (int x : arr ) System.out.print(x + ", ");
-				System.out.println();
 			}
-			if (!swapHappened) break;
+			if (!swapHappened)
+				break;
 		}
 		return arr;
 	}
